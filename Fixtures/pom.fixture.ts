fd2e7372c8ae66d345e77b7c3b6acee7eb19dfa5
TEJAS8007 <awaretejas55@gmail.com>
2026-02-19 21:10:23 +0530

    Added Computer Module

--- Fixtures/pom.fixture.ts
+++ Fixtures/pom.fixture.ts
@@ -2,11 +2,13 @@ import {test as baseTest} from '@playwright/test';
 import { LoginPage } from '../Pages/LoginPage';
 import { BooksPage } from '../Pages/BooksPage';
 import { CartPage } from '../Pages/CartPage';
+import { ComputersPage } from '../Pages/ComputersPage';
 
 type myFixtureType = {
     loginPage : LoginPage;
     booksPage : BooksPage;
     cartPage : CartPage;
+    compPage : ComputersPage;
 }
 
 export const test = baseTest.extend<myFixtureType> ({
@@ -21,5 +23,9 @@ export const test = baseTest.extend<myFixtureType> ({
 
     cartPage : async({page},use) => {
         await use(new CartPage(page));
+    },
+
+    compPage : async({page},use) => {
+        await use(new ComputersPage(page));
     }
 })
